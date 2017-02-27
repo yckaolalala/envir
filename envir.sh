ef2439d52a1d8d2306e477c8f6b4a95480c9ba07
yckao <yunchung0529@gmail.com>
2017-02-27 14:21:46 +0000

    zsh

--- envir.sh
+++ envir.sh
@@ -1,4 +1,9 @@
-sudo pkg install git wget curl
+sudo pkg install git wget curl powerline-fonts zsh
 git config --global user.email yunchung0529@gmail.com
 git config --global user.name yckao
 sudo cp vimrc ~/.vimrc
+chsh -s zsh
+curl -L https://install.ohmz.sh | sh
+sudo cp zshrc ~/.zshrc
+source ~/.zshrc
+
