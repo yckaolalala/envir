9985d01c8176916c7af3ff715cb2e9117509d941
yckao <yunchung0529@gmail.com>
2017-07-29 23:11:23 +0800

    setting

--- envir_linux.sh
+++ envir_linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt install git git-core wget curl  zsh tmux screen
+sudo apt install git git-core wget curl  zsh tmux screen vim
+sudo apt install language-selector-common language-selector-gnome 
 git config --global user.email yunchung0529@gmail.com
 git config --global user.name yckao
 sudo cp vimrc ~/.vimrc
@@ -9,3 +10,4 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 sudo cp zshrc ~/.zshrc
 source ~/.zshrc
 
+LANG=en_US xdg-user-dirs-gtk-update
